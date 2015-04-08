3270797bf0d6b6ef3a13e1e5b95353d48148e676
Herbefol <herbefol@gmail.com>
2015-04-08 16:41:33 +0200

    Commit enum et exception

---
+++ src/biblio/metier/BiblioException.java
@@ -0,0 +1,12 @@
+package biblio.metier;
+
+public class BiblioException extends Exception {
+	
+	public BiblioException () {
+	}
+	
+	public BiblioException(String message) {
+		super(message);
+	}
+
+}
